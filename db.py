ecc7ceb844c7411468bf88c886f963f0fbda2e24
Nnamdi <nnamdiau@gmail.com>
2026-07-17 14:44:48 +0100

    db.py

--- db.py
+++ db.py
@@ -2,17 +2,18 @@ import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
-
 load_dotenv()
+
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-client = MongoClient(DATABASE_URL)
+database = MongoClient(DATABASE_URL)
+
+
 
 def ping():
     try:
-        client.admin.command("ping")
-        print("Connected to the database successfully")
-    except Exception as e:
-        print("Could not connect to the database")
-        print(e)
+        database.admin.command('ping')
+        print("connected to the database successfully")
+    except:
+        print("could not connect to the database")
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from db import ping
 
 
+
+
 app = FastAPI()
 
 @app.on_event("startup")
